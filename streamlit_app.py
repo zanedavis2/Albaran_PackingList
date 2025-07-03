5c5bdcfe7b3ad734acf4d45705e0b5951e0c430c
zanedavis2 <zdavis11@elon.edu>
2025-07-03 15:14:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -214,7 +214,7 @@ if doc_input:
         st.subheader(f"Shipping Info for {albaran_df.loc[row_idx, 'docNumber']}")
         st.write(f"**Client**: {company_name}")
         st.write(f"**Email**: {contact_email}")
-        st.write(f"**Phone**: {contact_phone}  **Mobile**: {contact_mobile}")
+        st.write(f"**Phone**: {contact_phone}        **Mobile**: {contact_mobile}")
         st.write(f"**Billing Address**: {bill_address_str}")
 
         result_df = explode_order_row(albaran_df, row_idx, catalog_lookup=catalog_lookup)
